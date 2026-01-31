d432149d083287a9c86b5e413786e79c3fd091a1
Masahiro <masahiro19970823@yahoo.co.jp>
2026-02-01 00:57:56 +0900

    ストレージアカウント処理追加

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -1,5 +1,7 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from azure.storage.blob import BlobServiceClient
+from azure.identity import DefaultAzureCredential
 
 @app.route('/')
 def show_entries():
@@ -11,11 +13,12 @@ def show_entries():
 def login():
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
-            print('ユーザ名が異なります')
+            flash('ユーザ名が異なります')
         elif request.form['password'] != app.config['PASSWORD']:
-            print('パスワードが異なります')
+            flash('パスワードが異なります')
         else:
             session['logged_in'] = True
+            flash('ログインしました')
             return redirect('/')
         
     return render_template('login.html')
@@ -24,3 +27,12 @@ def login():
 def logout():
     session.pop('logged_in', None)
     return redirect('/')
+
+@app.route('/azure')
+def azure():
+    account_url = "https://flaskonazure2026.blob.core.windows.net"
+    default_credential = DefaultAzureCredential()
+    blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+    container_name = "blog2"
+    container_client = blob_service_client.create_container(container_name)
+    return render_template('/azure.html')
